bf21c4ebfcbf6327926c30429c79262bab19e2f7
TL <thomakleduc@gmail.com>
2018-11-10 17:45:05 +0100

    Accès fichier XML (nouveau projet : Connexion): Lecture du XML: # Question - Propositions
    • Réponse

---
+++ Connexion/lireXML.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Xml;
+
+namespace Connexion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // numéro de la question
+            int numeroQuestion = 1;
+            // Lecture du fichier XML
+            XmlDocument questionnaire = new XmlDocument();
+            questionnaire.Load("questionnaire.xml");
+
+            // Selectionne la question
+            XmlNode question = questionnaire.GetElementsByTagName("question")[numeroQuestion];
+
+            // Donne le titre de la question
+            XmlNode titreQuestion = question.SelectSingleNode("titreQuestion");
+            Console.WriteLine("# "+ titreQuestion.InnerText);
+
+            //Donne les propositions de la question
+            XmlNodeList propositions = question.SelectNodes("proposition");
+            foreach (XmlNode propos in propositions)
+            {
+                Console.WriteLine("- "+propos.InnerText);
+
+            }
+            //// Donne la réponse de la question
+            XmlNode reponse = question.SelectSingleNode("reponse");
+            Console.WriteLine("\n• "+ reponse.InnerText); 
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
