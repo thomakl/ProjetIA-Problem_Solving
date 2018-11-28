2e6caa31563910e0e65cf1c351faaaf5dc908607
Katell <katell.maro@gmail.com>
2018-11-28 09:32:58 +0100

    [Partie2] Modifs minimes

--- PartieII/Dijstra/FormDijkstra.Designer.cs
+++ PartieII/Dijstra/FormDijkstra.Designer.cs
@@ -49,11 +49,13 @@
             this.lblInstrArbre = new System.Windows.Forms.Label();
             this.btnAbandonListe = new System.Windows.Forms.Button();
             this.btnAbandonArbre = new System.Windows.Forms.Button();
+            this.lblDij = new System.Windows.Forms.Label();
+            this.lblEnonceDij = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtBoxFrep
             // 
-            this.txtBoxFrep.Location = new System.Drawing.Point(69, 208);
+            this.txtBoxFrep.Location = new System.Drawing.Point(69, 249);
             this.txtBoxFrep.Multiline = true;
             this.txtBoxFrep.Name = "txtBoxFrep";
             this.txtBoxFrep.ReadOnly = true;
@@ -62,7 +64,7 @@
             // 
             // txtBoxOrep
             // 
-            this.txtBoxOrep.Location = new System.Drawing.Point(272, 208);
+            this.txtBoxOrep.Location = new System.Drawing.Point(272, 249);
             this.txtBoxOrep.Multiline = true;
             this.txtBoxOrep.Name = "txtBoxOrep";
             this.txtBoxOrep.ReadOnly = true;
@@ -71,14 +73,14 @@
             // 
             // txtBoxFsaisie
             // 
-            this.txtBoxFsaisie.Location = new System.Drawing.Point(69, 324);
+            this.txtBoxFsaisie.Location = new System.Drawing.Point(69, 355);
             this.txtBoxFsaisie.Name = "txtBoxFsaisie";
             this.txtBoxFsaisie.Size = new System.Drawing.Size(162, 20);
             this.txtBoxFsaisie.TabIndex = 2;
             // 
             // txtBoxOsaisie
             // 
-            this.txtBoxOsaisie.Location = new System.Drawing.Point(272, 324);
+            this.txtBoxOsaisie.Location = new System.Drawing.Point(272, 355);
             this.txtBoxOsaisie.Name = "txtBoxOsaisie";
             this.txtBoxOsaisie.Size = new System.Drawing.Size(162, 20);
             this.txtBoxOsaisie.TabIndex = 3;
@@ -86,7 +88,7 @@
             // lblF
             // 
             this.lblF.AutoSize = true;
-            this.lblF.Location = new System.Drawing.Point(104, 183);
+            this.lblF.Location = new System.Drawing.Point(104, 224);
             this.lblF.Name = "lblF";
             this.lblF.Size = new System.Drawing.Size(93, 13);
             this.lblF.TabIndex = 4;
@@ -95,7 +97,7 @@
             // lblO
             // 
             this.lblO.AutoSize = true;
-            this.lblO.Location = new System.Drawing.Point(305, 183);
+            this.lblO.Location = new System.Drawing.Point(305, 224);
             this.lblO.Name = "lblO";
             this.lblO.Size = new System.Drawing.Size(99, 13);
             this.lblO.TabIndex = 5;
@@ -103,7 +105,7 @@
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(156, 368);
+            this.btnOk.Location = new System.Drawing.Point(156, 399);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(75, 23);
             this.btnOk.TabIndex = 6;
@@ -113,7 +115,7 @@
             // 
             // btnInit
             // 
-            this.btnInit.Location = new System.Drawing.Point(272, 368);
+            this.btnInit.Location = new System.Drawing.Point(272, 399);
             this.btnInit.Name = "btnInit";
             this.btnInit.Size = new System.Drawing.Size(75, 23);
             this.btnInit.TabIndex = 7;
@@ -124,7 +126,7 @@
             // lblEnonce
             // 
             this.lblEnonce.AutoSize = true;
-            this.lblEnonce.Location = new System.Drawing.Point(57, 42);
+            this.lblEnonce.Location = new System.Drawing.Point(57, 33);
             this.lblEnonce.Name = "lblEnonce";
             this.lblEnonce.Size = new System.Drawing.Size(253, 13);
             this.lblEnonce.TabIndex = 9;
@@ -132,7 +134,7 @@
             // 
             // treeViewSaisie
             // 
-            this.treeViewSaisie.Location = new System.Drawing.Point(334, 432);
+            this.treeViewSaisie.Location = new System.Drawing.Point(334, 457);
             this.treeViewSaisie.Name = "treeViewSaisie";
             this.treeViewSaisie.Size = new System.Drawing.Size(121, 97);
             this.treeViewSaisie.TabIndex = 10;
@@ -143,7 +145,7 @@
             // 
             this.lblArbre.AutoSize = true;
             this.lblArbre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblArbre.Location = new System.Drawing.Point(94, 388);
+            this.lblArbre.Location = new System.Drawing.Point(94, 413);
             this.lblArbre.Name = "lblArbre";
             this.lblArbre.Size = new System.Drawing.Size(125, 17);
             this.lblArbre.TabIndex = 12;
@@ -153,14 +155,14 @@
             // listBoxEnonce
             // 
             this.listBoxEnonce.FormattingEnabled = true;
-            this.listBoxEnonce.Location = new System.Drawing.Point(348, 36);
+            this.listBoxEnonce.Location = new System.Drawing.Point(348, 27);
             this.listBoxEnonce.Name = "listBoxEnonce";
             this.listBoxEnonce.Size = new System.Drawing.Size(120, 95);
             this.listBoxEnonce.TabIndex = 13;
             // 
             // textBoxDepart
             // 
-            this.textBoxDepart.Location = new System.Drawing.Point(50, 102);
+            this.textBoxDepart.Location = new System.Drawing.Point(50, 88);
             this.textBoxDepart.Name = "textBoxDepart";
             this.textBoxDepart.ReadOnly = true;
             this.textBoxDepart.Size = new System.Drawing.Size(100, 20);
@@ -169,7 +171,7 @@
             // lblDepart
             // 
             this.lblDepart.AutoSize = true;
-            this.lblDepart.Location = new System.Drawing.Point(57, 86);
+            this.lblDepart.Location = new System.Drawing.Point(57, 72);
             this.lblDepart.Name = "lblDepart";
             this.lblDepart.Size = new System.Drawing.Size(93, 13);
             this.lblDepart.TabIndex = 15;
@@ -178,7 +180,7 @@
             // lblArrive
             // 
             this.lblArrive.AutoSize = true;
-            this.lblArrive.Location = new System.Drawing.Point(207, 86);
+            this.lblArrive.Location = new System.Drawing.Point(207, 72);
             this.lblArrive.Name = "lblArrive";
             this.lblArrive.Size = new System.Drawing.Size(88, 13);
             this.lblArrive.TabIndex = 17;
@@ -186,7 +188,7 @@
             // 
             // textBoxArrive
             // 
-            this.textBoxArrive.Location = new System.Drawing.Point(200, 102);
+            this.textBoxArrive.Location = new System.Drawing.Point(200, 88);
             this.textBoxArrive.Name = "textBoxArrive";
             this.textBoxArrive.ReadOnly = true;
             this.textBoxArrive.Size = new System.Drawing.Size(100, 20);
@@ -194,7 +196,7 @@
             // 
             // btnOkArbre
             // 
-            this.btnOkArbre.Location = new System.Drawing.Point(308, 551);
+            this.btnOkArbre.Location = new System.Drawing.Point(308, 576);
             this.btnOkArbre.Name = "btnOkArbre";
             this.btnOkArbre.Size = new System.Drawing.Size(75, 23);
             this.btnOkArbre.TabIndex = 18;
@@ -205,7 +207,7 @@
             // 
             // txtBoxSaisieArb
             // 
-            this.txtBoxSaisieArb.Location = new System.Drawing.Point(130, 509);
+            this.txtBoxSaisieArb.Location = new System.Drawing.Point(130, 534);
             this.txtBoxSaisieArb.Name = "txtBoxSaisieArb";
             this.txtBoxSaisieArb.Size = new System.Drawing.Size(101, 20);
             this.txtBoxSaisieArb.TabIndex = 20;
@@ -213,7 +215,7 @@
             // 
             // lblInstrArbre
             // 
-            this.lblInstrArbre.Location = new System.Drawing.Point(104, 414);
+            this.lblInstrArbre.Location = new System.Drawing.Point(104, 439);
             this.lblInstrArbre.Name = "lblInstrArbre";
             this.lblInstrArbre.Size = new System.Drawing.Size(169, 88);
             this.lblInstrArbre.TabIndex = 21;
@@ -224,7 +226,7 @@
             // 
             // btnAbandonListe
             // 
-            this.btnAbandonListe.Location = new System.Drawing.Point(213, 418);
+            this.btnAbandonListe.Location = new System.Drawing.Point(213, 449);
             this.btnAbandonListe.Name = "btnAbandonListe";
             this.btnAbandonListe.Size = new System.Drawing.Size(75, 23);
             this.btnAbandonListe.TabIndex = 22;
@@ -234,7 +236,7 @@
             // 
             // btnAbandonArbre
             // 
-            this.btnAbandonArbre.Location = new System.Drawing.Point(414, 551);
+            this.btnAbandonArbre.Location = new System.Drawing.Point(414, 576);
             this.btnAbandonArbre.Name = "btnAbandonArbre";
             this.btnAbandonArbre.Size = new System.Drawing.Size(75, 23);
             this.btnAbandonArbre.TabIndex = 23;
@@ -243,11 +245,33 @@
             this.btnAbandonArbre.Visible = false;
             this.btnAbandonArbre.Click += new System.EventHandler(this.btnAbandonArbre_Click);
             // 
+            // lblDij
+            // 
+            this.lblDij.AutoSize = true;
+            this.lblDij.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDij.Location = new System.Drawing.Point(57, 147);
+            this.lblDij.Name = "lblDij";
+            this.lblDij.Size = new System.Drawing.Size(146, 17);
+            this.lblDij.TabIndex = 24;
+            this.lblDij.Text = "Algorithme de Dijkstra";
+            // 
+            // lblEnonceDij
+            // 
+            this.lblEnonceDij.Location = new System.Drawing.Point(66, 170);
+            this.lblEnonceDij.Name = "lblEnonceDij";
+            this.lblEnonceDij.Size = new System.Drawing.Size(423, 29);
+            this.lblEnonceDij.TabIndex = 25;
+            this.lblEnonceDij.Text = "Pour noter les noeuds, remplir les deux champs en notant tous les noeuds à chaque" +
+    " étape et en les séparant par une virgule. A chaque étape, cliquer sur \"Valider\"" +
+    ".";
+            // 
             // FormDijkstra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(548, 624);
+            this.Controls.Add(this.lblEnonceDij);
+            this.Controls.Add(this.lblDij);
             this.Controls.Add(this.btnAbandonArbre);
             this.Controls.Add(this.btnAbandonListe);
             this.Controls.Add(this.lblInstrArbre);
@@ -299,6 +323,8 @@
         private System.Windows.Forms.Label lblInstrArbre;
         private System.Windows.Forms.Button btnAbandonListe;
         private System.Windows.Forms.Button btnAbandonArbre;
+        private System.Windows.Forms.Label lblDij;
+        private System.Windows.Forms.Label lblEnonceDij;
     }
 }
 
